6532485d0dff88b3e77cc0de1b1159020d5a379f
Khaled <116715044+KhaledEldfry@users.noreply.github.com>
2025-06-11 01:22:31 +0300

    Update main.py

--- Graduation_last/main.py
+++ Graduation_last/main.py
@@ -1,27 +1,10 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import StreamingResponse
-from ultralytics import YOLO
-import cv2
-import numpy as np
-from io import BytesIO
-from PIL import Image
+from app.predict import run_prediction
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-model = YOLO("best.pt") 
-
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    contents = await file.read()
-    np_image = np.frombuffer(contents, np.uint8)
-    image = cv2.imdecode(np_image, cv2.IMREAD_COLOR)
-
-    results = model(image)
-    result_img = results[0].plot()
-
-    pil_img = Image.fromarray(cv2.cvtColor(result_img, cv2.COLOR_BGR2RGB))
-    buffer = BytesIO()
-    pil_img.save(buffer, format="JPEG")
-    buffer.seek(0)
-
-    return StreamingResponse(buffer, media_type="image/jpeg")
+    result = await run_prediction(file)
+    return JSONResponse(content=result)
