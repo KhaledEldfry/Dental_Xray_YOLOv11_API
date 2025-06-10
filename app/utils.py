948eeee8f953153d08024a25727beea2b4ed6ad0
Khaled <116715044+KhaledEldfry@users.noreply.github.com>
2025-06-11 01:56:57 +0300

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,12 +1,4 @@
-import torch
-from PIL import Image
-import numpy as np
-from io import BytesIO
-
-def get_yolov11_model():
-    return torch.hub.load("WongKinYiu/yolov11", "custom", path="app/models/best.pt", source="local")
-
-async def load_image(file):
-    image_bytes = await file.read()
-    image = Image.open(BytesIO(image_bytes)).convert("RGB")
-    return np.array(image)
+async def save_upload_file(upload_file, destination):
+    with open(destination, "wb") as buffer:
+        content = await upload_file.read()
+        buffer.write(content)
